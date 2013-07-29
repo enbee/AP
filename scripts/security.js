22e8aa8b9a93702d1bcfdd77a1885a716b62b2c8
Nick Burchall <nick@enbee-design.co.uk>
2013-07-29 15:35:50 +0100

    Alerts

--- scripts/security.js
+++ scripts/security.js
@@ -29,6 +29,7 @@ function handleLogin() {
 	navigator.notification.alert("click");
 	if(u != '' && p!= '') {
         $.post("http://asgt.mocwebservices.co.uk/pg/login.php", {username:u,password:p}, function(res) {
+        	alert(res);
             if(res == true) {
 				//store
 				window.localStorage["username"] = u;
