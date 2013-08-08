09ed13bd66cce7f5bb1f75ee0a42822a25a29770
Nick Burchall <nick@enbee-design.co.uk>
2013-08-08 16:46:54 +0100

    GPS

--- scripts/angelsec.js
+++ scripts/angelsec.js
@@ -13,7 +13,29 @@ $().ready(function() {
 	// --- CORDOVA READY
 	document.addEventListener("deviceready", onDeviceReady, false);
 	function onDeviceReady() {
-	
+		
+		// Get GPS
+		navigator.geolocation.getCurrentPosition(onGpsSuccess, onGpsError);
+		
+		function onGpsSuccess(position) {
+			alert('Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          + new Date(position.timestamp)          + '<br />');
+		}
+		
+		
+		function onGpsError(error) {
+			alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+		}
+		
+		
+		
 		// CHECK USER LOGGED IN	
 		if(authorised != 'true'|| authorised == undefined){
 			page('#authorise');
