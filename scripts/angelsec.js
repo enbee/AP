64a25be63f0ea48680097750f717f950b3519613
Nick Burchall <nick@enbee-design.co.uk>
2013-08-15 11:23:12 +0100

    0

--- scripts/angelsec.js
+++ scripts/angelsec.js
@@ -215,7 +215,7 @@ $( "#reportForm" ).on( "submit", function( event ) {
 		
 		var reportArray = JSON.stringify($(form).serializeArray());
 		
-		console.log('Form Name: '+reportArray);
+		console.log('Form Name: '+reportArray[0]);
 		
 		/* var formName = reportArray[0].formName; */
 		//alert(reportArray.array[0].formName);
